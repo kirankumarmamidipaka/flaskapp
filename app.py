8a937a961697d31e5fd54964e6f94180e52673fd
kirankumarmamidipaka <kiran.mp123@gmail.com>
2021-06-30 01:35:22 +0530

    Flask app tested

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 
 from flask import Flask, render_template, request, flash, redirect,url_for, jsonify, session 
 from flask import Response,send_file
-
+from email_validator import validate_email, EmailNotValidError
+from string import *
 app = Flask(__name__)
 
 import db as db
@@ -9,43 +10,67 @@ import db as db
 @app.route('/')
 def index():
     
-    return None
+    return '{ "Message":"welcome"}'
 
 @app.route('/adduser',methods = ['GET','POST'])
 def add_user():
     request_data = request.json
-    email = request_data["email"]
-    firstname = request_data["firstname"]
-    lastname = request_data["lastname"]
-    password = request_data["password"]
-    db.add_user(email,firstname,lastname,password)
+    email = request_data["email"].strip()
+    firstname = request_data["firstname"].strip()
+    lastname = request_data["lastname"].strip()
+    password = request_data["password"].strip()
+    if email == "" or firstname == "" or lastname == "" or password == "":
+        response = Response("All fields are mandatory", 400, mimetype='application/json')
+        return response
+    try:
+        valid = validate_email(email)
+        email = valid.email
+    except EmailNotValidError as e:
+        response = Response("Enter valid email.", 400, mimetype='application/json')
+        return response 
+    err=db.add_user(email,firstname,lastname,password)
+    if err:
+        response = Response("User already exists or try with other email id", 400, mimetype='application/json')
+        return response 
     response = Response("User added", 200, mimetype='application/json')
     return response
 
 @app.route('/users',methods = ['GET'])
 def get_all_users():
         details = {'Users':db.get_all_users()}
-        print(details)
-        #for detail in details:
-         #   var = detail
-        #return render_template('index.html',var=details)
+        if details == None or "" or {}:
+            response = Response("No Users found.", 404, mimetype='application/json')
+            return response
         return details
 @app.route('/user/<email>', methods=['GET'])
 def get_user(email):
-    print("hello im working")
     user_details=db.get_user(email)
     if user_details == None:
-        return "User not found"
+        response = Response("No User found.", 404, mimetype='application/json')
+        return response
     return user_details
 
 @app.route('/user/<email>', methods=['PUT'])
 def update_user(email):
-
+    
+    user_details=db.get_user(email)
+    if user_details == None:
+        response = Response("User not found to update.", 404, mimetype='application/json')
+        return response
     request_data = request.get_json()
-    email = request_data["email"]
-    firstname = request_data["firstname"]
-    lastname = request_data["lastname"]
-    password = request_data["password"]
+    email = request_data["email"].strip()
+    firstname = request_data["firstname"].strip()
+    lastname = request_data["lastname"].strip()
+    password = request_data["password"].strip()
+    if email == "" or firstname == "" or lastname == "" or password == "":
+        response = Response("All fields are mandatory", 400, mimetype='application/json')
+        return response
+    try:
+        valid = validate_email(email)
+        email = valid.email
+    except EmailNotValidError as e:
+        response = Response("Enter valid email.", 400, mimetype='application/json')
+        return response 
     db.update_user(email,firstname,lastname,password)
     response = Response("User Updated", status=200, mimetype='application/json')
     return response
@@ -53,6 +78,11 @@ def update_user(email):
 @app.route('/user/<email>',methods=['DELETE'])
 
 def delete_user(email):
+    
+    user_details=db.get_user(email)
+    if user_details == None:
+        response = Response("User not found to delete.", 404, mimetype='application/json')
+        return response
     db.delete_user(email)
     response = Response("User deleted", status=200, mimetype='application/json')
     return response
--- db.py
+++ db.py
@@ -14,9 +14,13 @@ conn = pymysql.connect(
 
 
 def add_user(email,firstname,lastname,password):
-    cur=conn.cursor()
-    cur.execute("INSERT INTO users (email,firstname,lastname,password) VALUES (%s,%s,%s,%s)", (email,firstname,lastname,password))
-    conn.commit()
+    try:
+        cur=conn.cursor()
+        cur.execute("INSERT INTO users (email,firstname,lastname,password) VALUES (%s,%s,%s,%s)", (email,firstname,lastname,password))
+        conn.commit()
+    except pymysql.IntegrityError as err:
+        return err
+
 
 def get_user(email):
     cur=conn.cursor()
@@ -44,18 +48,21 @@ def update_user(email,firstname,lastname,password):
 
 def delete_user(email):
 
-    cur=conn.cursor()
-    cur.execute("delete from users where email=%s",email)
-    conn.commit() 
+    try:
+        cur=conn.cursor()
+        cur.execute("delete from users where email=%s",email)
+        conn.commit() 
+    except pymysql.IntegrityError as err:
+        return err
 
 
 
 
-#delete_user("kiran123@gmail.com")
-#add_user("kiran123@gmail.com","kiran","kumar","pwdddd")
+print(delete_user("kiran123@gmail.com"))
+#print(add_user("kiran123@gmail.com","kiran","kumar","pwdddd"))
 #add_user("ramesh123@gmail.com","ramesh","kumar","1234")
 #add_user("suresh@gmail.com","suresh","kumar","243555")
-print(get_user("kiran12ghg3@gmail.com"))
+#print(get_user("kiran12ghg3@gmail.com"))
 #print(get_all_users())
 #print(update_user("kiran123@gmail.com","kiran","kalyan","pwdddd1234"))
 #print(update_user("kiran123@gmail.com","kiran","kalyan","pwdddd"))
